c4c6025915c7f3312c8507832d4d6eb6ff792f43
greicens <gsilva@mail.ccsf.edu>
2017-01-15 01:05:22 -0800

    adds songs in spanish

--- server.js
+++ server.js
@@ -16,23 +16,55 @@ var songs = [
          {
           english: "My tea's gone cold, I wondering why I got out of bed at all.",
           portuguese: "Meu chá esfriou, estou me perguntando o porquê de eu sequer ter saído da cama."
+          spanish:"Mi té se ha enfriado y me pregunto por qué me salí de la cama hoy"
         },
          {
           english: "The morning rain clouds up my window, and I can't see at all.",
           portuguese:"A chuva da manhã embaça a minha janela, e eu não posso ver nada.",
-        },
-        {
+          spanish:"La lluvia de la mañana nubla mi ventana y no puedo ver nada"
+        }],
+        verse2: [{
           english: "And even if I could it'd all be grey, but your picture on my wall.",
           portuguese:"E mesmo se eu pudesse seria tudo cinza, menos a sua foto na minha parede.",
-
+          spanish:"Y aun si pudiera, todo se vería gris pero tú fotografía en mi pared"
         },
         {
           english: "It reminds me that it's not so bad, it's not so bad",
           portuguese:"Ela me lembra que não é tão ruim. Não é tão ruim",
+          spanish:"Me recuerda que no es tan malo no es tan malo"
 
-        }
-      ]
-      }]
+        }]
+      },
+      { artistName: 'Adele',
+        album: 'Hello',
+        title: 'Hello',
+        difficulty: 'intermediate',
+        albumCoverUrl: 'images/hello.png',
+        verse1:[
+           {
+             english:"Hello, it's me . I was wondering if after all these years you'd like to meet to go over everything",
+             portuguese:"Olá Sou eu eu estava imaginando se após todos esses anos Você gostaria que nos encontrássemos para superarmos tudo"
+             spanish:"Hola soy yo Me preguntaba si después de todos estos años te gustaría que quedásemos para analizarlo todo "
+           },
+           {
+             english:"They say that time's supposed to heal ya but I ain't done much healing",
+             portuguese:"Dizem que o tempo supostamente lhe cura mas eu ainda não fui completamente curada",
+             spanish:"Dicen que el tiempo se supone que cura pero a mí no me ha curado demasiado. "
+           }],
+        verse2:[
+          {
+            english:"Hello, can you hear me. I'm in California dreaming about who we used to be",
+            portuguese:"Olá Você pode me ouvir?Estou na Califórnia sonhando com quem costumávamos ser",
+            spanish:"¿Hola puedes oírme? Estoy en California soñando con lo que solíamos ser cuando éramos más jóvenes y libres "
+          },
+          {
+            english:"When we were younger and free I've forgotten how it felt before the world fell at our feet",
+            portuguese:"Quando éramos mais jovens E livres Eu esqueci como era antes do mundo cair aos nossos pés",
+            spanish:"He olvidado cómo me sentía ante el mundo antes de que cayera a nuestros pies "
+          }
+        ]
+
+    ]
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
