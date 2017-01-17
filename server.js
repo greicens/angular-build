6c7a2506dfe109b0e562b0667e7fe5197a5c28f9
greicens <gsilva@mail.ccsf.edu>
2017-01-16 17:44:36 -0800

    updates adele video

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ var songs = [
     title: 'Hello',
     difficulty: 'intermediate',
     albumCoverUrl: 'images/hello.png',
-    iframeUrl: 'https://www.youtube.com/embed/9h0Arg_-380',
+    iframeUrl: 'https://www.youtube.com/embed/YQHsXMglC9A',
     lines:[
        {
          english:"Hello, it's me . I was wondering if after all these years you'd like to meet to go over everything",
